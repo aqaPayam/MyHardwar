6876ee6c0fa73cf627c58f94c98e7b1dc6fa0e9c
aqaPayam <93112601+aqaPayam@users.noreply.github.com>
2023-06-01 10:10:14 +0330

    Update predictor.v

--- predictor.v
+++ predictor.v
@@ -1,5 +1,24 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
 
-// Your code
+reg [1:0] state;
+
+always @(clk) begin
+
+    if (result)
+    begin
+        if(taken)
+            state = &state ? state : state + 1;
+        else 
+            state = |state ? state -1 : state ;
+    end
+
+    if(request)
+    begin
+        prediction = state[1];
+    end
+
+
+end
+
 
 endmodule
